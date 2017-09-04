ca65d60dc699c3720470d32d1636f6c61aa4ce6b
Matt Tilley <mtilley255@gmail.com>
2017-09-04 11:05:50 -0500

    One of several attempts - not working.

--- primary.js
+++ primary.js
@@ -1,4 +1,23 @@
-var countdown = function(pass)
+var white_ball_generator = function()
+{
+	var white_balls = [];
+	
+	if (white_balls.length < 5)
+	{
+		var possible = Math.ceil(Math.random) * 69;
+		white_balls.push[possible];
+		console.log(white_balls);
+	}
+	
+	
+}
+
+white_ball_generator();
+
+
+
+
+/*var countdown = function(pass)
 {
 	var pass = prompt("ENTER A NUMBER");
 	
@@ -7,10 +26,8 @@ var countdown = function(pass)
 		console.log(pass);
 		pass--;
 	}
-}
-
-
-countdown();
+}*/
+/*countdown();*/
 
 
 
